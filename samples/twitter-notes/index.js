a2b6144cc86a61821f47cac90004fac167961b52
Gaurav Jain <gaurav@Gauravs-MacBook-Pro.local>
2017-02-25 11:26:14 +0800

    Initial commit

---
+++ samples/twitter-notes/index.js
@@ -0,0 +1,17 @@
+var cluster = require('cluster');
+var app = require('./app');
+
+if (cluster.isMaster) {
+	// // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
+
+    //gaurav - use one cpu for now for troubleshooting other issues
+    cpuCount = 1;
+
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+} else {
+	app();
+}
