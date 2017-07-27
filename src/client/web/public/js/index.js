efbfd14e65da1d7a7a6f3df7b9ee55fc4978866c
Gaurav Jain <gauravjain.nsit@gmail.com>
2017-07-27 11:46:56 +0800

    Bug Fix: Home page feeds not showing up

--- src/client/web/public/js/index.js
+++ src/client/web/public/js/index.js
@@ -1,29 +1,9 @@
 $(document).ready(function(){
-	$("#postChallenge").click(postChallenge);
-
 	createLoginHeader();
 
 	setupFeed();
 });
 
 function setupFeed() {
-
-	createAndAppendContentContainer($("#feed"), "feed", [{type: "feed"}], [{type: "date", url: "/api/feeds"}]);
-	//setupChallenges();
-
-	//setupEntries();
-
-}
-
-function setupChallenges() {
-	createAndAppendContentContainer($("#feed"), "challenges", [{type: "filmstrip"}], [{type: "date", url: "/api/challenges?sortBy=date&count=6"}]);
-}
-
-function setupEntries() {
-	createAndAppendContentContainer($("#feed"), "entries", [{type: "filmstrip"}], [{type: "date", url: "/api/entries?sortBy=date&count=6"}]);
-}
-
-
-function postChallenge() {
-	window.open("/newchallenge", "_self");
+	createAndAppendContentContainer($("#feed"), 0, "feed", [{type: "feed"}], [{type: "date", url: "/api/feeds"}]);
 }
