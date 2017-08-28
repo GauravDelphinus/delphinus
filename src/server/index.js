4dfdfd91000cd70f66601e6cafc933920a96e60f
Gaurav Jain <gauravjain.nsit@gmail.com>
2017-08-28 12:00:51 +0800

    Temporary logging to files

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,24 @@
 var cluster = require('cluster');
 var app = require('./app');
+var fs = require("fs");
+var config = require("./config");
+
+var logDir = config.path.logFolder;
+var access = fs.createWriteStream(logDir + '/node.access.log', { flags: 'a' })
+      , error = fs.createWriteStream(logDir + '/node.error.log', { flags: 'a' });
+
+// redirect stdout / stderr
+process.stdout.pipe(access);
+process.stderr.pipe(error);
+
+process.stdout.write = access.write.bind(access);
+process.stderr.write = error.write.bind(error);
+
+process.on('uncaughtException', function(err) {
+  console.error((err && err.stack) ? err.stack : err);
+});
+
+console.log("testing");
 
 if (cluster.isMaster) {
         // // Count the machine's CPUs
