80ae1ea30338891c51c3ccf8b6587e86d573d290
Gaurav Jain <gauravjain.nsit@gmail.com>
2017-10-26 20:14:39 +0800

    fix

--- deployment/scripts/setup_stage.sh
+++ deployment/scripts/setup_stage.sh
@@ -1,4 +1,4 @@
 #Setup on staging - only required once (not for every check-in) for setting up directories, links, etc.
 #Call from src/server (or similar level) directory
 
-pm2 deploy ../../deployment/aws/ecosystem.config.js production setup
+pm2 deploy ../../deployment/aws/ecosystem.config.js staging setup
