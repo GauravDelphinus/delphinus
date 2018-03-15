8bc4a5ede7cef0d98f89412529ca3ff515fc3e42
Gaurav Jain <gauravdelphinus@gmail.com>
2018-03-15 08:37:27 +0800

    CircleCI script: called setup_dirs and launch app post setup

--- src/server/imageProcessor.js
+++ src/server/imageProcessor.js
@@ -9,7 +9,7 @@ module.exports = {
 	applyStepsToImage : function(sourceImage, targetImage, imageType, steps, caption, next) {
 		//logger.debug("applyStepsToImage: sourceImage: " + sourceImage + ", targetImage: " + targetImage + ", steps: " + JSON.stringify(steps));
 		if (targetImage) {
-			if (fs.existsSync(targetImage)) {
+			if (fs.existsSync(targetImage)) { //check if file already exists in Cache
 				return next(0, targetImage);
 			} else {
 				applySteps(sourceImage, targetImage, steps, caption, next);
